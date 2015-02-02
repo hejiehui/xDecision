90bc858ec8e4982f27071af56fada4050111f159
He, Jiehui <he_jiehui@163.com>
2015-02-02 09:59:09 +0800

    make inner impl outside

--- com.xross.tools.xdecision/src/com/xross/tools/xdecision/FactorParser.java
+++ com.xross.tools.xdecision/src/com/xross/tools/xdecision/FactorParser.java
@@ -2,16 +2,4 @@ package com.xross.tools.xdecision;
 
 public interface FactorParser {
 	Object parse(String name, String value);
-
-	/**
-	 * Default implementation which just use the string as value
-	 * @author jhhe
-	 */
-	public class StringFactorParser implements FactorParser {
-		@Override
-		public Object parse(String name, String value) {
-			return value;
-		}
-		
-	}
 }
--- com.xross.tools.xdecision/src/com/xross/tools/xdecision/Facts.java
+++ com.xross.tools.xdecision/src/com/xross/tools/xdecision/Facts.java
@@ -1,29 +1,6 @@
 package com.xross.tools.xdecision;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public interface Facts {
 	Object get(String name);
-	
-	/**
-	 * Default implementation, which use a map internally to hold the factory values
-	 * @author jhhe
-	 */
-	public class DefaultFacts implements Facts {
-		private Map<String, Object> values = new HashMap<String, Object>();
-
-		@Override
-		public Object get(String name) {
-			return values.get(name);
-		}
-		
-		public void set(String name, Object value) {
-			values.put(name, value);
-		}
-		
-		public void reset() {
-			values.clear();
-		}
-	}
 }
