38aeb6be59a3776d8926704e366b0af1d728be09
hejiehui <he_jiehui@163.com>
2022-02-16 00:15:54 +0800

    allow change element type from contect menu in outline panel

--- com.xrosstools.xdecision.editor/src/com/xrosstools/xdecision/editor/NamedElementContextMenuProvider.java
+++ com.xrosstools.xdecision.editor/src/com/xrosstools/xdecision/editor/NamedElementContextMenuProvider.java
@@ -1,16 +1,19 @@
 package com.xrosstools.xdecision.editor;
 
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 
 import com.xrosstools.xdecision.editor.actions.CommandAction;
 import com.xrosstools.xdecision.editor.actions.DecisionTreeMessages;
 import com.xrosstools.xdecision.editor.actions.InputTextCommandAction;
 import com.xrosstools.xdecision.editor.commands.ChangeElementNameCommand;
+import com.xrosstools.xdecision.editor.commands.ChangeElementTypeCommand;
 import com.xrosstools.xdecision.editor.commands.DeleteElementCommand;
 import com.xrosstools.xdecision.editor.model.NamedElement;
 import com.xrosstools.xdecision.editor.model.NamedElementContainer;
 import com.xrosstools.xdecision.editor.model.NamedElementTypeEnum;
+import com.xrosstools.xdecision.editor.model.NamedType;
 import com.xrosstools.xdecision.editor.model.XrossEvaluatorConstants;
 import com.xrosstools.xdecision.editor.parts.NamedElementTreePart;
 
@@ -21,13 +24,34 @@ public class NamedElementContextMenuProvider implements XrossEvaluatorConstants,
         this.editor = editor;
     }
     
-    public void buildContextMenu(IMenuManager menu, NamedElementTreePart namedTypePart) {
-        NamedElement element = (NamedElement)namedTypePart.getModel();
-        NamedElementTypeEnum type = ((NamedElementContainer<?>)namedTypePart.getParent().getModel()).getElementType();
+    public void buildContextMenu(IMenuManager menu, NamedElementTreePart namedElementPart) {
+        NamedElement element = (NamedElement)namedElementPart.getModel();
+        
+        NamedElementTypeEnum type = ((NamedElementContainer<?>)namedElementPart.getParent().getModel()).getElementType();
         String typeName = type.getTypeName();
         
         menu.add(new InputTextCommandAction(editor, String.format(CHANGE_NAME_MSG, typeName), "New Name", element.getName(), new ChangeElementNameCommand(element)));
+        
+        changeTypeMenu(menu, element);
+        
         menu.add(new Separator());
-        menu.add(new CommandAction(editor, String.format(REMOVE_MSG, element.getName()), false, new DeleteElementCommand((NamedElementContainer<NamedElement>)namedTypePart.getParent().getModel(), element)));
+        menu.add(new CommandAction(editor, String.format(REMOVE_MSG, element.getName()), false, new DeleteElementCommand((NamedElementContainer<NamedElement>)namedElementPart.getParent().getModel(), element)));
     }
+    
+    public void changeTypeMenu(IMenuManager menu, NamedElement element) {
+        String[] qualifiedTypes = element.getElementType().getQualifiedDataTypes();
+        if(qualifiedTypes.length == 0)
+            return;
+        
+        String category = element.getElementType().getTypeName();
+        MenuManager changeTypeMenu = new MenuManager(String.format(CHANGE_TYPE_MSG, category));
+        
+        NamedType nameType = (NamedType)element;
+        for(String typeName: qualifiedTypes) {
+            changeTypeMenu.add(new CommandAction(editor, typeName, nameType.getType().getName().equals(typeName), new ChangeElementTypeCommand(nameType, typeName)));
+        }
+        
+        menu.add(changeTypeMenu);
+    }
+
 }
--- com.xrosstools.xdecision.editor/src/com/xrosstools/xdecision/editor/commands/ChangeElementTypeCommand.java
+++ com.xrosstools.xdecision.editor/src/com/xrosstools/xdecision/editor/commands/ChangeElementTypeCommand.java
@@ -7,16 +7,18 @@ import com.xrosstools.xdecision.editor.model.NamedType;
 
 public class ChangeElementTypeCommand extends Command{
     private NamedType typeNameSupported;
+    private String newTypeName;
     private DataType oldType;
     private DataType newType;
     
-    public ChangeElementTypeCommand(NamedType typeNameSupported, DataType newType){
+    public ChangeElementTypeCommand(NamedType typeNameSupported, String newTypeName){
         this.typeNameSupported = typeNameSupported;
+        this.newTypeName = newTypeName;
         this.oldType = typeNameSupported.getType();
-        this.newType = newType;
     }
     
     public void execute() {
+        newType = DataType.findDataType(newTypeName);
         redo();
     }
 
