ed7796f68b4198a4f7d97698954b4f2bb3475e65
He, Jiehui <he_jiehui@163.com>
2023-11-30 13:31:21 +0800

    optimize interface to support backward compatibility

--- com.xrosstools.xdecision.idea.editor/src/com/xrosstools/xdecision/idea/editor/NewDecisionTreeAction.java
+++ com.xrosstools.xdecision.idea.editor/src/com/xrosstools/xdecision/idea/editor/NewDecisionTreeAction.java
@@ -3,9 +3,7 @@ package com.xrosstools.xdecision.idea.editor;
 import com.xrosstools.idea.gef.DefaultNewModelFileAction;
 
 public class NewDecisionTreeAction extends DefaultNewModelFileAction {
-    private static final String template = getTemplate(NewDecisionTreeAction.class, "/templates/template.xdecision");
-
     public NewDecisionTreeAction() {
-        super("Xross Decision Tree", XdecisionFileType.EXTENSION, XdecisionsIcons.TREE, "new_xdecision_file", template);
+        super("Xross Decision Tree", XdecisionFileType.EXTENSION, XdecisionsIcons.TREE, "new_xdecision_file", "/templates/template.xdecision");
     }
 }
