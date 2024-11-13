01760754c6b3c2854da88ae9ad8d4237cb4295ba
He, Jiehui <he_jiehui@163.com>
2024-11-13 16:54:43 +0800

    Update AllTests.java

--- com.xrosstools.xdecision.sample/src/main/java/com/xrosstools/xdecision/AllTests.java
+++ com.xrosstools.xdecision.sample/src/main/java/com/xrosstools/xdecision/AllTests.java
@@ -6,25 +6,24 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.xrosstools.xdecision.ext.ExpressionCompilerTest;
 import com.xrosstools.xdecision.ext.TokenParserTest;
-import com.xrosstools.xdecision.sample.Chose_collection_method_v1_Test;
-import com.xrosstools.xdecision.sample.Chose_collection_method_v2_Test;
-import com.xrosstools.xdecision.sample.ExpressionTest;
-import com.xrosstools.xdecision.sample.SampleTest;
-import com.xrosstools.xdecision.sample.SampleV2Test;
-import com.xrosstools.xdecision.sample.XrossEvaluatorTest;
-import com.xrosstools.xdecision.sample.XrossEvaluatorUnitTest;
+import com.xrosstools.xdecision.sample.*;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        ExpressionCompilerTest.class,
-        TokenParserTest.class,
-        Chose_collection_method_v1_Test.class,
-        Chose_collection_method_v2_Test.class,
-        ExpressionTest.class,
-        SampleTest.class,
-        SampleV2Test.class,
-        XrossEvaluatorTest.class,
-        XrossEvaluatorUnitTest.class,
+    ExpressionCompilerTest.class,
+    TokenParserTest.class,
+    Chose_collection_method_v1_Test.class,
+    Chose_collection_method_v2_Test.class,
+    DecisionRuleTest.class,
+    ExpressionTest.class,
+    NumberComparationTest.class,
+    ObjectValidationTest.class,
+    RangeValidationTest.class,
+    SampleTest.class,
+    SampleV2Test.class,
+    StringValidationTest.class,
+    XrossEvaluatorTest.class,
+    XrossEvaluatorUnitTest.class,
 })
 public class AllTests  {
 
